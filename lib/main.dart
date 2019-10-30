67df7819dff6cac30fe82c216224259bde547d65
jeeeanlefchak <jean_2103@hotmail.com>
2019-10-30 20:37:24 -0300

    K'

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:whats/Home.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main(){
+  WidgetsFlutterBinding.ensureInitialized();
+  Firestore.instance
+  .collection("usuarios")
+  .document("001")
+  .setData({"nome": "jean"});
+  runApp(MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
